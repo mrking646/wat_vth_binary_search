55f21f79b6d70d84dcb1f1320251a5589277c510
onezhenji <hellozhenji@gmail.com>
2022-11-17 18:48:17 +0800

    literally, completed HCI class

--- main.py
+++ main.py
@@ -46,41 +46,106 @@ class HCIstress:
         kw_only=True,
     )
 
+
+
 @attrs.define
 class Resource:
-    
+    device: str
     GATE : list
     DRAIN: list
     SOURCE: list
     BULK: list
 
 @attrs.define
+class VtlinSweep:
+    resource: Resource
+    V_force_D: float
+    V_force_S: float
+    V_force_B: float
+    I_compl_G: float
+    I_compl_D: float
+    I_compl_S: float
+    I_compl_B: float
+    V_step_G: float
+    V_start_G: float
+    V_stop_G: float
+
+
+@attrs.define
+class VtSatSweep:
+    resource: Resource
+    V_force_D: float
+    V_force_S: float
+    V_force_B: float
+    I_compl_G: float
+    I_compl_D: float
+    I_compl_S: float
+    I_compl_B: float
+    V_step_G: float
+    V_start_G: float
+    V_stop_G: float
+
+
+@attrs.define
+class IbMaxSweep:
+    resource: Resource
+    V_force_D: float
+    V_force_S: float
+    V_force_B: float
+    I_compl_G: float
+    I_compl_D: float
+    I_compl_S: float
+    I_compl_B: float
+    V_step_G: float
+    V_start_G: float
+    V_stop_G: float
+
+
+@attrs.define
 class HCIsweep:
     resource: Resource
+    description: str
+    Vtlin: VtlinSweep
+    VtSat: VtSatSweep
+    IbMax: IbMaxSweep
+
 
     dieCoord: str = attrs.field(
         default='A1',
         kw_only=True,
     )
+
     tileName: str = attrs.field(
         default='A1',
         kw_only=True,
-
     )
+
+    # vStart : float = attrs.field(
+    #     default=0,
+    #     kw_only=True,
+    # )
+    # vStop : float = attrs.field(
+    #     default=0,
+    #     kw_only=True,
+    # )
+    # vStep : float = attrs.field(
+    #     default=0,
+    #     kw_only=True,
+    # )
 
     # biases: list[chnVoltBias] = attrs.field(
     #     validator=attrs.validators.deep_iterable(member_validator=attrs.validators.instance_of((chnVoltBias)))
     # )
 
-    sourceDelay: float = attrs.field(
-        default=1e-3,
-        kw_only=True,
-    )
+    # sourceDelay: float = attrs.field(
+    #     default=1e-3,
+    #     kw_only=True,
+    # )
 
-    apertureTime: float = attrs.field(
-        default=1e-3,
-        kw_only=True,
-    )
+    # apertureTime: float = attrs.field(
+    #     default=1e-3,
+    #     kw_only=True,
+    # )
     
 def populate():
     wb = Workbook()    
@@ -97,7 +162,7 @@ def runHCI():
 def dumpExceltoPython():
     wb = load_workbook(filename='test_plan_HCI_1110_V3.xlsx')
     ws = wb.active
-    dDut, dTile, dTest = {}, {}, {}
+    dDut, dHCItest, dTest_Vtlin, dTest_VtSat, dTest_IbMAX, dResource = {}, {}, {}, {}, {}, {}
     header = []
     rows_buf = []
     count = 0
@@ -109,42 +174,96 @@ def dumpExceltoPython():
             rows_buf.append(row)
         count += 1
     df_plan = pd.DataFrame(rows_buf, columns=header)
+    norepeat_df_plan = df_plan.drop_duplicates(subset=[' dut.name'])
+    # count_1 = 0
+    sDuts = set()
+
+    # Get all the DUTs and resources
+    count = 0
+    for dut in norepeat_df_plan.loc[:, ' dut.name']:
+        padGATE = df_plan.loc[count, ' dut.pads[G]']
+        padDRAIN = df_plan.loc[count, ' dut.pads[D]']
+        padSOURCE = df_plan.loc[count, ' dut.pads[S]']
+        padBULK = df_plan.loc[count, ' dut.pads[B]']
+        GATE = [f'{wiring[(padGATE-1)+24*i][1][0][wiring[(padGATE-1)+24*i][1][1]]}/{wiring[(padGATE-1)+24*i][1][2]}' for i in range(4)]
+        DRAIN  = [f'{wiring[(padDRAIN-1)+24*i][1][0][wiring[(padDRAIN-1)+24*i][1][1]]}/{wiring[(padDRAIN-1)+24*i][1][2]}' for i in range(4)]
+        SOURCE = [f'{wiring[(padSOURCE-1)+24*i][1][0][wiring[(padSOURCE-1)+24*i][1][1]]}/{wiring[(padSOURCE-1)+24*i][1][2]}' for i in range(4)]
+        BULK = [f'{wiring[(padBULK-1)+24*i][1][0][wiring[(padBULK-1)+24*i][1][1]]}/{wiring[(padBULK-1)+24*i][1][2]}' for i in range(4)]
+        dResource[dut] = Resource(dut, GATE, DRAIN, SOURCE, BULK)
+        sDuts.add(dut)
+        count += 1
+    print(dResource)
 
     count = 0
     for c in df_plan.loc[:, 'fullname']:
+        dut = df_plan.loc[count, ' dut.name']
         if 'Vtlin' in c:
-            padGATE = df_plan.loc[count, ' dut.pads[G]']
-            padDRAIN = df_plan.loc[count, ' dut.pads[D]']
-            padSOURCE = df_plan.loc[count, ' dut.pads[S]']
-            padBULK = df_plan.loc[count, ' dut.pads[B]']
-            GATE = [f'{wiring[(padGATE-1)+24*i][1][0][wiring[(padGATE-1)+24*i][1][1]]}/{wiring[(padGATE-1)+24*i][1][2]}' for i in range(4)]
-            DRAIN  = [f'{wiring[(padDRAIN-1)+24*i][1][0][wiring[(padDRAIN-1)+24*i][1][1]]}/{wiring[(padDRAIN-1)+24*i][1][2]}' for i in range(4)]
-            SOURCE = [f'{wiring[(padSOURCE-1)+24*i][1][0][wiring[(padSOURCE-1)+24*i][1][1]]}/{wiring[(padSOURCE-1)+24*i][1][2]}' for i in range(4)]
-            BULK = [f'{wiring[(padBULK-1)+24*i][1][0][wiring[(padBULK-1)+24*i][1][1]]}/{wiring[(padBULK-1)+24*i][1][2]}' for i in range(4)]
-
-            dDut[c] = Resource(GATE, DRAIN, SOURCE, BULK)
-            print(dDut[c])
-            dTest[c] = HCIsweep(dDut[c])
-            print(dTest[c])
+            vForceD = df_plan.loc[count,  ' test.V_force_D']
+            vForceS = df_plan.loc[count,  ' test.V_force_S']
+            vForceB = df_plan.loc[count,  ' test.V_force_B']
+            vStartG = df_plan.loc[count,  ' test.V_start_G']
+            vStopG = df_plan.loc[count,  ' test.V_stop_G']
+            vStepG = df_plan.loc[count,  ' test.V_step_G'] 
+            iComplG = df_plan.loc[count,  ' test.I_compl_G']
+            iComplD = df_plan.loc[count,  ' test.I_compl_D']
+            iComplS = df_plan.loc[count,  ' test.I_compl_S']
+            iComplB = df_plan.loc[count,  ' test.I_compl_B']
+            dTest_Vtlin[dut] = VtlinSweep(dResource[dut], vForceD, vForceS, vForceB, iComplG, iComplD, iComplS, iComplB, vStepG, vStartG, vStopG)
+        elif 'Vtsat' in c:
+            vForceD = df_plan.loc[count,  ' test.V_force_D']
+            vForceS = df_plan.loc[count,  ' test.V_force_S']
+            vForceB = df_plan.loc[count,  ' test.V_force_B']
+            vStartG = df_plan.loc[count,  ' test.V_start_G']
+            vStopG = df_plan.loc[count,  ' test.V_stop_G']
+            vStepG = df_plan.loc[count,  ' test.V_step_G'] 
+            iComplG = df_plan.loc[count,  ' test.I_compl_G']
+            iComplD = df_plan.loc[count,  ' test.I_compl_D']
+            iComplS = df_plan.loc[count,  ' test.I_compl_S']
+            iComplB = df_plan.loc[count,  ' test.I_compl_B']
+            dTest_VtSat[dut] = VtlinSweep(dResource[dut], vForceD, vForceS, vForceB, iComplG, iComplD, iComplS, iComplB, vStepG, vStartG, vStopG)
+        elif 'Ibmax' in c:
+            vForceD = df_plan.loc[count,  ' test.V_force_D']
+            vForceS = df_plan.loc[count,  ' test.V_force_S']
+            vForceB = df_plan.loc[count,  ' test.V_force_B']
+            vStartG = df_plan.loc[count,  ' test.V_start_G']
+            vStopG = df_plan.loc[count,  ' test.V_stop_G']
+            vStepG = df_plan.loc[count,  ' test.V_step_G'] 
+            iComplG = df_plan.loc[count,  ' test.I_compl_G']
+            iComplD = df_plan.loc[count,  ' test.I_compl_D']
+            iComplS = df_plan.loc[count,  ' test.I_compl_S']
+            iComplB = df_plan.loc[count,  ' test.I_compl_B']
+            dTest_IbMAX[dut] = VtlinSweep(dResource[dut], vForceD, vForceS, vForceB, iComplG, iComplD, iComplS, iComplB, vStepG, vStartG, vStopG)
+        # print(dTest[c])
         count += 1
+
+    print("hahha")
+    for dut in sDuts:
+        resource = dResource[dut]
+        Vtlin = dTest_Vtlin[dut]
+        VtSat = dTest_VtSat[dut]
+        Ibmax = dTest_IbMAX[dut]
+        dHCItest[dut] = HCIsweep(dResource[dut], dut, dTest_Vtlin[dut], dTest_VtSat[dut], dTest_IbMAX[dut])
+    
+    return dHCItest
+
 def populate(session):
     pass
 
 
-class HCI:
+# class HCI:
     
-    def __init__(self, fileName, sheetNames=['all']):
-        self.dut = None
+#     def __init__(self, fileName, sheetNames=['all']):
+#         self.dut = None
         
-    def prepareVtlin(self):
-        pass
+#     def prepareVtlin(self):
+#         pass
 
-    def prepareVtSat(self):
-        pass
+#     def prepareVtSat(self):
+#         pass
 
-    def prepareIbmax(self):
-        pass
+#     def prepareIbmax(self):
+#         pass
 
 
-dumpExceltoPython()
+print(dumpExceltoPython())
 
