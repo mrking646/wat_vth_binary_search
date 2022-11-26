40466763ae7bcadcd42cfa5f1fa61ef30fa1694d
onezhenji <hellozhenji@gmail.com>
2022-11-27 01:45:38 +0800

    complete HCI class

--- main.py
+++ main.py
@@ -174,17 +174,19 @@ def dumpExceltoPython():
             rows_buf.append(row)
         count += 1
     df_plan = pd.DataFrame(rows_buf, columns=header)
-    norepeat_df_plan = df_plan.drop_duplicates(subset=[' dut.name'])
+    norepeat_df_plan = df_plan.drop_duplicates(subset=[' dut.name']).reset_index()
     # count_1 = 0
     sDuts = set()
 
     # Get all the DUTs and resources
     count = 0
     for dut in norepeat_df_plan.loc[:, ' dut.name']:
-        padGATE = df_plan.loc[count, ' dut.pads[G]']
-        padDRAIN = df_plan.loc[count, ' dut.pads[D]']
-        padSOURCE = df_plan.loc[count, ' dut.pads[S]']
-        padBULK = df_plan.loc[count, ' dut.pads[B]']
+        # print(norepeat_df_plan.loc[:, ' dut.pads[G]'])
+        padGATE = norepeat_df_plan.loc[count, ' dut.pads[G]']
+        # print(f'Gate is {padGATE}')
+        padDRAIN = norepeat_df_plan.loc[count, ' dut.pads[D]']
+        padSOURCE = norepeat_df_plan.loc[count, ' dut.pads[S]']
+        padBULK = norepeat_df_plan.loc[count, ' dut.pads[B]']
         GATE = [f'{wiring[(padGATE-1)+24*i][1][0][wiring[(padGATE-1)+24*i][1][1]]}/{wiring[(padGATE-1)+24*i][1][2]}' for i in range(4)]
         DRAIN  = [f'{wiring[(padDRAIN-1)+24*i][1][0][wiring[(padDRAIN-1)+24*i][1][1]]}/{wiring[(padDRAIN-1)+24*i][1][2]}' for i in range(4)]
         SOURCE = [f'{wiring[(padSOURCE-1)+24*i][1][0][wiring[(padSOURCE-1)+24*i][1][1]]}/{wiring[(padSOURCE-1)+24*i][1][2]}' for i in range(4)]
@@ -192,7 +194,7 @@ def dumpExceltoPython():
         dResource[dut] = Resource(dut, GATE, DRAIN, SOURCE, BULK)
         sDuts.add(dut)
         count += 1
-    print(dResource)
+    # print(dResource)
 
     count = 0
     for c in df_plan.loc[:, 'fullname']:
@@ -236,34 +238,52 @@ def dumpExceltoPython():
         # print(dTest[c])
         count += 1
 
-    print("hahha")
+    # print("hahha")
     for dut in sDuts:
         resource = dResource[dut]
         Vtlin = dTest_Vtlin[dut]
         VtSat = dTest_VtSat[dut]
         Ibmax = dTest_IbMAX[dut]
         dHCItest[dut] = HCIsweep(dResource[dut], dut, dTest_Vtlin[dut], dTest_VtSat[dut], dTest_IbMAX[dut])
-    
     return dHCItest
+
+def runStress(sess, dHCItest):
+    sess.source_mode = nidcpower.SourceMode.SINGLE_POINT
+    sess.voltage_level_autorange = True
+
+# would it be fine if we dump all channels of 4163 into a session and we then open relays we don't need?
+def common_settings(**HCI):
+    sess = nidcpower.Session(resource_name='SMU1, SMU2', channels=range(0, 24), reset=False, options={'Simulate': True, 'DriverSetup': 'Model:4163; BoardType:PXIe'})
+    sess.voltage_level_autorange = True
+    sess.current_limit_autorange = True
+    sess.output_function = nidcpower.OutputFunction.DC_VOLTAGE
+    sess.voltage_level = 0
+    sess.current_limit = 1e-3
+    sess.output_connected = False # turn off all channels 
+    sess.output_cutoff_enabled = True
+    return sess
+
+def constructVtlinSweeptest(sess, **HCI):
+    pass
+
+def constructVtSatSweeptest(sess, **HCI):
+    pass
+
+def constructIbMAXSweeptest(sess, **HCI):
+    pass
+
+
+def write_results(sess):
+    pass
+
 
 def populate(session):
     pass
 
 
-# class HCI:
-    
-#     def __init__(self, fileName, sheetNames=['all']):
-#         self.dut = None
-        
-#     def prepareVtlin(self):
-#         pass
-
-#     def prepareVtSat(self):
-#         pass
-
-#     def prepareIbmax(self):
-#         pass
+lstHCI = []
+dHCItest = dumpExceltoPython()
+print(common_settings(**dHCItest))
 
 
-print(dumpExceltoPython())
 
